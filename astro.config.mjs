03099dc834f1a7790b53da0ed302eac8e13bfa22
srinivas-webdev <srinivas.javascript@gmail.com>
2026-07-27 16:52:56 +0530

    initial changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,41 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Docs with Tailwind',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-			customCss: ['./src/styles/global.css'],
+			title: 'Learn Spoken English',
+        sidebar: [
+            {
+                label: 'Daily English',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Intro', slug: 'daily-english' },
+                    { label: 'Simple Past in Polite Offers', slug: 'daily-english/simple-past-in-polite-offers' },
+                ],
+            },
+            {
+                label: 'Contractions',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'contractions/wh-contractions' },
+                ],
+            },
+            {
+                label: 'Reference',
+                items: [{ autogenerate: { directory: 'reference' } }],
+            },
+        ],
+        customCss: ['./src/styles/global.css'],
+        components: {
+            // Replace the default title component
+            PageTitle: './src/components/overrides/CustomTitle.astro',
+        }
 		}),
+		vue()
 	],
 	vite: {
 		plugins: [tailwindcss()],
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,107 @@
 import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { docsLoader, i18nLoader } from '@astrojs/starlight/loaders';
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { z } from 'astro/zod';
+import { file } from "astro/loaders";
+
+const mediaSchema = z.object({
+  url: z.string(),
+  publicId: z.string().optional(),
+  type: z.string()
+})
+
+const mediaArraySchema = z.preprocess(
+  (val) => {
+    // If it's already an array, return it; otherwise try to parse JSON
+    if (Array.isArray(val)) return val;
+    if (typeof val === 'string') {
+      try {
+				return JSON.parse(val);
+      } catch (e) {
+        throw new Error("Invalid JSON string");
+      }
+    }
+    return val;
+  },
+  z.array(mediaSchema)
+);
+
+const examplesArraySchema = z.preprocess(
+  (val) => {
+    // If it's already an array, return it; otherwise try to parse JSON
+    if (Array.isArray(val)) return val;
+    if (typeof val === 'string') {
+      try {
+				return JSON.parse(val);
+      } catch (e) {
+        throw new Error("Invalid JSON string");
+      }
+    }
+    return val;
+  },
+  z.array(z.string())
+);
+
+const meaningSchema = z.object({
+  name: z.string(),
+  examples: examplesArraySchema.optional(),
+  media: mediaArraySchema.optional()
+})
+
+
+const meaningsArraySchema = z.preprocess(
+  (val) => {
+    // If it's already an array, return it; otherwise try to parse JSON
+    if (Array.isArray(val)) return val;
+    if (typeof val === 'string') {
+      try {
+				return JSON.parse(val);
+      } catch (e) {
+        throw new Error("Invalid JSON string");
+      }
+    }
+    return val;
+  },
+  z.array(meaningSchema)
+);
+
+const extraDetailsArraySchema = z.preprocess(
+  (val) => {
+    // If it's already an array, return it; otherwise try to parse JSON
+    if (Array.isArray(val)) return val;
+    if (typeof val === 'string') {
+      if (val.length === 0) return []
+      try {
+				return JSON.parse(val);
+      } catch (e) {
+        throw new Error("Invalid JSON string");
+      }
+    }
+    return val;
+  },
+  z.array(z.tuple([z.string(), z.array(z.string())]))
+);
+
+const phraseDetailsSchema = z.object({
+  id: z.coerce.number().optional(),
+  name: z.string(),
+  type: z.coerce.number().optional(),
+  origin: z.string().optional(),
+	examples: examplesArraySchema.optional(),
+  meanings: meaningsArraySchema,
+  extraDetails: extraDetailsArraySchema.optional()
+})
+
+
+
+const phrase = defineCollection({
+  loader: file("src/data/phrases.json"),
+	schema: phraseDetailsSchema
+});
+
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
+	phrase
 };
